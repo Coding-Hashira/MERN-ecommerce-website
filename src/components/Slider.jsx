32d7e4e0ac982c6975c5a4ecfb8bd4dba20c506a
Coding-Hashira <96920778+Coding-Hashira@users.noreply.github.com>
2022-03-19 19:34:05 +0530

    After completing slider and adding my own data

---
+++ src/components/Slider.jsx
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
+import { ArrowForwardIosOutlined } from "@mui/icons-material";
+import { sliderItems } from "../data";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+`;
+
+const Arrow = styled.div`
+  &:hover {
+    opacity: 0.7;
+  }
+  width: 50px;
+  height: 50px;
+  z-index: 90;
+  transition: 150ms;
+  position: absolute;
+  top: 0;
+  opacity: 0.5;
+  bottom: 0;
+  margin: auto;
+  background-color: #f9f9f9;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  cursor: pointer;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(
+    ${(props) =>
+      props.slideIndex === 1 ? 100 : props.slideIndex === 2 ? -100 : 0}vw
+  );
+  transition: all 1.5s ease;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+  height: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  height: ${(props) => props.height};
+  position: absolute;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+  z-index: 10;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(1);
+
+  useEffect(() => {
+    console.log(slideIndex);
+  }, [slideIndex]);
+
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex < 3 ? slideIndex + 1 : 1);
+    } else {
+      setSlideIndex(slideIndex > 1 ? slideIndex - 1 : 3);
+    }
+  };
+
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowBackIosNewOutlinedIcon />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item?.bg}>
+            <ImgContainer>
+              <Image height={item?.height} src={item?.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item?.title}</Title>
+              <Desc>{item?.desc}</Desc>
+              <Button>SHOP NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowForwardIosOutlined />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/2e056443-6505-46ef-aa88-e20e2778b0a9.png?v=1625046016",
+    title: "FLAT 35% ON TOP HEADPHONES",
+    desc: "LISTEN TO YOUR FAVORITE MUSIC WITH TOP QUALITY HEADPHONES WHILE CODING",
+    bg: "fdf5f5",
+    height: "75%",
+  },
+  {
+    id: 2,
+    img: "https://images.samsung.com/is/image/samsung/in-top-mount-freezer-rt65k7058bs-rt65k7058bs-tl-black-243405539?$720_576_PNG$",
+    desc: "FLAT 30% OFF ON SAMSUNG REFREGERATORS",
+    bg: "f0dffb",
+    title: "STAY COOL WITH SAMSUNG FRIDGES",
+    height: "75%",
+  },
+  {
+    id: 3,
+    img: "https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1613470608.10126241.png",
+    desc: "HURRY UP, OFFER ENDING SOON!",
+    height: "100%",
+    title: "GET MIN. 25% OFF ON REDMI 9",
+    bg: "f0f7fb",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+];

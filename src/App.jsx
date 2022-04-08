9619d9b29c87cc19c617796573dfc0b0c5434e3f
Coding-Hashira <96920778+Coding-Hashira@users.noreply.github.com>
2022-04-08 19:17:28 +0530

    After Redesigning Register Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
-import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "./app.css";
 
 const App = () => {
   return (
     <div>
-      <Login />
+      <Register />
       Waheguru
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,26 +3,34 @@ import styled from "styled-components";
 
 const Container = styled.div`
   width: 100vw;
+  overflow-x: hidden;
   height: 100vh;
-  background: linear-gradient(
-      rgba(255, 255, 255, 0.5),
-      rgba(255, 255, 255, 0.5)
-    ),
-    url("https://static.vecteezy.com/system/resources/previews/005/008/960/large_2x/information-connection-communication-on-virtual-interface-technology-background-abstract-technology-background-hi-tech-communication-concept-futuristic-digital-innovation-background-vector.jpg")
-      center; //TODO: Add A Better Background Image
+  text-align: center;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+  position: absolute;
+`;
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0;
+  width: 100%;
 `;
 
 const Wrapper = styled.div`
-  padding: 20px;
-  width: 40%;
-  border-radius: 20px;
-  background-color: white;
+  overflow-x: hidden;
   display: flex;
+  width: 40%;
+  padding: 10px;
   flex-direction: column;
   align-items: center;
+`;
+
+const Left = styled.div`
+  width: calc(60vw - 1px);
+  display: flex;
+  justify-content: center;
 `;
 
 const Title = styled.h1`
@@ -34,6 +42,7 @@ const Form = styled.form`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  padding: 0 50px;
 `;
 
 const Input = styled.input`
@@ -46,12 +55,10 @@ const Input = styled.input`
   }
 `;
 
-const Agreement = styled.span`
-  font-size: 12px;
-  margin: 20px 0;
-`;
+const Link = styled.a`
+  text-decoration: none;
+  font-weight: bold;
 
-const Link = styled.b`
   cursor: pointer;
   &:hover {
     text-decoration: underline;
@@ -61,7 +68,7 @@ const Link = styled.b`
 const Button = styled.button`
   width: 40%;
   border: none;
-  padding: 15px 20px;
+  padding: 13px 20px;
   background-color: teal;
   color: white;
   cursor: pointer;
@@ -73,11 +80,28 @@ const Button = styled.button`
   }
 `;
 
+const Line = styled.div`
+  height: 50%;
+  width: 1px;
+  background-color: gray;
+  margin: auto;
+  position: relative;
+`;
+
+const Image = styled.img`
+  height: 300px;
+  object-fit: contain;
+`;
+
 const Login = () => {
   return (
     <Container>
+      <Left>
+        <Image src="/register.svg" />
+      </Left>
+      <Line></Line>
       <Wrapper>
-        <Title>CREATE AN ACCOUNT</Title>
+        <Title>Sign In</Title>
         <Form>
           <Input placeholder="Name" />
           <Input placeholder="Last Name" />
@@ -89,7 +113,7 @@ const Login = () => {
             By creating an account, I consent to the processing of my personal
             data in according with the <Link>PRIVACY POLICY</Link>
           </Agreement>
-          <Button>CREATE</Button>
+          <Button>LOGIN</Button>
         </Form>
       </Wrapper>
     </Container>
